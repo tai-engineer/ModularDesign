52fcbd7fac91f73e5a69b8096bc3cda55666c6df
TaiVan <vcmtspkt@gmail.com>
2024-08-27 17:10:11 +0700

    Update CharacterPhysics
    - Move all physic calculations to each specific state in state machine

--- Assets/Scripts/Character/CharacterPhysics.cs
+++ Assets/Scripts/Character/CharacterPhysics.cs
@@ -10,28 +10,12 @@ namespace Character.Physics
     {
         [SerializeField] InputReaderSO _input;
         
-        Vector3 _moveVector;
-        Vector2 _moveInput;
-        bool _jumpInput;
-        float _verticalMovement;
-
-        [SerializeField]
-        float _moveSpeed = 5f;
-        float _gravity; // Gravity has negative value
+        [NonSerialized] public Vector3 moveVector;
         
-        [SerializeField]
-        float _jumpForce = 5f;
-
-        // [SerializeField]
-        // float _maxFallSpeed = -50f;
-        //
-        // [SerializeField]
-        // float _maxRiseSpeed = 100f;
-        
-        public float gravityMultiplier = 5f;
-
         #region Properties
         public bool IsGrounded { get; private set; }
+        public Vector2 MoveInput { get; private set; }
+        public bool JumpInput { get; private set; }
 
         #endregion
         
@@ -55,62 +39,25 @@ namespace Character.Physics
             _groundDetector = GetComponent<GroundDetector>();
         }
 
-        void Start()
-        {
-            _gravity = UnityEngine.Physics.gravity.y;
-        }
-
         void FixedUpdate()
         {
-            _rb.MovePosition(transform.position + _moveVector * Time.deltaTime);
+            _rb.MovePosition(transform.position + moveVector * Time.deltaTime);
         }
 
         void Update()
         {
             GroundCheck();
-            GroundMovement();
-            AirborneMovement();
-            Debug.Log($"_moveVector: {_moveVector}, IsGrounded: {IsGrounded}");
+            Debug.Log($"_moveVector: {moveVector}, IsGrounded: {IsGrounded}");
         }
 
-        void Jump()
-        {
-            _verticalMovement = _jumpForce;
-        }
         void GroundCheck()
         {
             IsGrounded = _groundDetector.Check();
         }
-        void AirborneMovement()
-        {
-            if (IsGrounded && _jumpInput)
-            {
-                Jump();
-            }
-            else if (IsGrounded)
-            {
-                _verticalMovement = 0;
-            }
-            else
-            {
-                _verticalMovement += _gravity * gravityMultiplier * Time.deltaTime;
-                // _verticalMovement = Mathf.Clamp(_verticalMovement, _maxFallSpeed, _maxRiseSpeed);
-            }
-
-            _moveVector.y = _verticalMovement;
-        }
-
-        void GroundMovement()
-        {
-            
-            var movement = _moveSpeed * _moveInput;
-            _moveVector = new Vector3(movement.x, _moveVector.y, movement.y);
-        }
-
         #region Callback
 
-        void OnMove(Vector2 moveInput) => _moveInput = moveInput;
-        void OnJump(bool jumpInput) => _jumpInput = jumpInput;
+        void OnMove(Vector2 moveInput) => MoveInput = moveInput;
+        void OnJump(bool jumpInput) => JumpInput = jumpInput;
         #endregion
     }
 }
