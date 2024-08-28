9763c4de26c0dd0595707ee848c1bc017d874cbc
TaiVan <vcmtspkt@gmail.com>
2024-08-28 20:01:26 +0700

    Update InputActionMap
    - CameraRotate ( Mouse Delta, IJKL keys)
    - Run (Shift key)
    - MouseControlCamera (RMB)

--- Assets/Scripts/Input/InputReaderSO.cs
+++ Assets/Scripts/Input/InputReaderSO.cs
@@ -6,18 +6,27 @@ public class InputReaderSO : ScriptableObject, GameControls.IGamePlayActions
 {
     public event Action<Vector2> OnMoveEvent = delegate { };
     public event Action<bool> OnJumpStartEvent = delegate { };
+    public event Action startedRunning = delegate { };
+    public event Action stoppedRunning = delegate { };
+    public event Action enableMouseControlCameraEvent = delegate { };
+    public event Action disableMouseControlCameraEvent = delegate { };
+    public event Action<Vector2> cameraMoveEvent = delegate { };
 
     GameControls _controls;
     void OnEnable()
     {
-        _controls ??= new GameControls();
-        _controls.GamePlay.SetCallbacks(this);
+        if (_controls == null)
+        {
+            _controls = new GameControls();
+            _controls.GamePlay.SetCallbacks(this);
+        }
+        
         _controls.GamePlay.Enable();
     }
 
     void OnDisable()
     {
-        _controls?.GamePlay.Disable();
+        _controls.GamePlay.Disable();
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -28,5 +37,35 @@ public class InputReaderSO : ScriptableObject, GameControls.IGamePlayActions
     public void OnJump(InputAction.CallbackContext context)
     {
         OnJumpStartEvent.Invoke(context.ReadValueAsButton());
+    }
+
+    public void OnMouseControlCamera(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            enableMouseControlCameraEvent.Invoke();
+        }
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            disableMouseControlCameraEvent.Invoke();
+        }
+    }
+
+    public void OnRotateCamera(InputAction.CallbackContext context)
+    {
+        cameraMoveEvent.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Performed:
+                startedRunning.Invoke();
+                break;
+            case InputActionPhase.Canceled:
+                stoppedRunning.Invoke();
+                break;
+        }
     }
 }
